c14ecbb06997049ca11ff740758de944575bc4e1
Jakob Vesely <jakob@appartig.at>
2021-03-26 13:23:02 +0100

    Init Angular Application

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ffgfuea-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ffgfuea';
+}
