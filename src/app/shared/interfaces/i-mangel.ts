d00bb74e4b4b5331e94123eb6d50cded3ef94016
Jakob Vesely <jakob@appartig.at>
2021-04-05 14:40:22 +0200

    Mangel Model Things

--- src/app/shared/interfaces/i-mangel.ts
+++ src/app/shared/interfaces/i-mangel.ts
@@ -1,11 +1,12 @@
-import { IPerson } from './i-person';
-
 export interface IMangel
 {
     id?: number;
     flascheId: number;
     datetime: Date;
     personId: number;
+    typeBereich: string;
+    typeDescription: string;
     note: string;
+    ereignisId: number;
     datetimeFixed: Date;
 }
--- src/app/shared/models/mangel-of-flasche/mangel-of-flasche.ts
+++ src/app/shared/models/mangel-of-flasche/mangel-of-flasche.ts
@@ -8,15 +8,21 @@ export class MangelOfFlasche implements IMangel
     public datetime: Date;
     public personId: number;
     public person: Person;
+    public typeBereich: string;
+    public typeDescription: string;
     public note: string;
+    public ereignisId: number;
     public datetimeFixed: Date = null;
 
-    constructor(flascheId: number = null, datetime: Date = new Date(), personId: number = null, note: string = '', datetimeFixed: Date = null, id?: number)
+    constructor(flascheId: number = null, datetime: Date = new Date(), personId: number = null, typeBereich: string = '', typeDescription: string = '', note: string = '', ereignisId: number = null, datetimeFixed: Date = null, id?: number)
     {
         this.flascheId = flascheId;
         this.datetime = datetime;
         this.personId = personId;
+        this.typeBereich = typeBereich;
+        this.typeDescription = typeDescription;
         this.note = note;
+        this.ereignisId = ereignisId;
         this.datetimeFixed = datetimeFixed;
         this.id = id;
     }
