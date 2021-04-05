9e7aa5b636b05f73310c92be8fab2d8a0ee9824b
Jakob Vesely <jakob@appartig.at>
2021-04-05 17:16:22 +0200

    Add Ereignisse Page, add open print dialog

--- src/app/pages/ereignisse-page/ereignisse-page.component.ts
+++ src/app/pages/ereignisse-page/ereignisse-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PrintReportsComponent } from 'src/app/shared/dialogs/print-reports/print-reports.component';
+import { EreignisType } from 'src/app/shared/enums/ereignis-type';
+import { Ereignis } from 'src/app/shared/models/ereignis/ereignis';
+import { EreignisseService } from 'src/app/shared/services/ereignisse/ereignisse.service';
 
 @Component({
     selector: 'ffg-ereignisse-page',
@@ -7,6 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EreignissePageComponent implements OnInit
 {
-    constructor() { }
-    public ngOnInit(): void { }
+    public EreignisType = EreignisType;
+    public ereignisse: Array<Ereignis> = [];
+
+    constructor(private ereignisseService: EreignisseService, public dialog: MatDialog) { }
+
+    public async ngOnInit(): Promise<void>
+    {
+        this.ereignisse = await (await this.ereignisseService.getAll()).reverse();
+    }
+
+    public async openPrintReportings(ereignis: Ereignis)
+    {
+        const dialog = this.dialog.open(PrintReportsComponent, { width: '500px', data: ereignis });
+    }
 }
