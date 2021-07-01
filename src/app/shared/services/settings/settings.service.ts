d47560dded8d76f83317848bea003baa3c67d543
Jakob Vesely <jakob@appartig.at>
2021-07-01 16:22:30 +0200

    Add Sync URL to Settings

--- src/app/shared/services/settings/settings.service.ts
+++ src/app/shared/services/settings/settings.service.ts
@@ -12,21 +12,25 @@ export class SettingsService
 
     public testMode = false;
     public barcodeScannerStartSequenz = '';
+    public syncUrl = '';
 
     public loadAll()
     {
         this.testMode = this.readBoolean('testMode');
         this.barcodeScannerStartSequenz = this.readString('barcodeScannerStartSequenz');
+        this.syncUrl = this.readString('syncUrl');
     }
 
     public saveAll()
     {
         this.writeTestMode();
         this.writeBarcodeScannerStartSequenz();
+        this.writeSyncURL();
     }
 
     public writeTestMode = () => this.write('testMode', this.testMode);
     public writeBarcodeScannerStartSequenz = () => this.write('barcodeScannerStartSequenz', this.barcodeScannerStartSequenz);
+    public writeSyncURL = () => this.write('syncUrl', this.syncUrl);
 
     private write(key: string, value: any)
     {
