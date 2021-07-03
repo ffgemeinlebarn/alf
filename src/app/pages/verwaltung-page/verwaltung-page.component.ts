885e83f23193eab8e04bef8796a14894d190b6f4
Jakob Vesely <jakob@appartig.at>
2021-07-03 11:50:42 +0200

    Adaptions auf Verwaltungs Page

--- src/app/pages/verwaltung-page/verwaltung-page.component.ts
+++ src/app/pages/verwaltung-page/verwaltung-page.component.ts
@@ -4,6 +4,8 @@ import { SyncService } from 'src/app/shared/services/sync/sync.service';
 import { StammdatenService } from 'src/app/shared/services/stammdaten/stammdaten.service';
 import { IFeuerwehr } from 'src/app/shared/interfaces/i-feuerwehr';
 import { IFlasche } from 'src/app/shared/interfaces/i-flasche';
+import { SettingsService } from 'src/app/shared/services/settings/settings.service';
+import { RemoveFeuerwehrComponent } from 'src/app/shared/dialogs/remove-feuerwehr/remove-feuerwehr.component';
 
 @Component({
     selector: 'ffg-verwaltung-page',
@@ -12,12 +14,26 @@ import { IFlasche } from 'src/app/shared/interfaces/i-flasche';
 })
 export class VerwaltungPageComponent implements OnInit
 {
-    public displayedColumns: string[] = ['karteiNr', 'geraeteNr', 'flaschennummer', 'barcode', 'typenBezeichnung', 'typenInformation'];
+    public displayedColumns: string[] = ['karteiNr', 'geraeteNr', 'flaschennummer', 'barcode', 'typenBezeichnung', 'typenInformation', 'lastEdit'];
     public feuerwehrNummerToSync = null;
 
     public selectedFeuerwehr: IFeuerwehr = null;
     public selectedFlasche: IFlasche = null;
 
-    constructor(public dialog: MatDialog, public sync: SyncService, public stammdaten: StammdatenService) { }
+    constructor(
+        public dialog: MatDialog,
+        public sync: SyncService,
+        public stammdaten: StammdatenService,
+        public settings: SettingsService) { }
     public async ngOnInit(): Promise<void> { }
+
+    public addToSync(feuerwehrNummer: number)
+    {
+        this.sync.addFeuerwehr(feuerwehrNummer);
+    }
+
+    public openRemoveFeuerwehr(feuerwehr: IFeuerwehr)
+    {
+        this.dialog.open(RemoveFeuerwehrComponent, { width: '500px', data: feuerwehr });
+    }
 }
