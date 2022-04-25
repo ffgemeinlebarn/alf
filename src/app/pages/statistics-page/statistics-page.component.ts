13f159e7a507a7ce7b28fe4771b8ab03eef55b0c
Jakob Vesely <jakob@appartig.at>
2022-04-25 23:25:16 +0200

    Simple Stats Implementation

--- src/app/pages/statistics-page/statistics-page.component.ts
+++ src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EreignisseService } from 'src/app/shared/services/ereignisse/ereignisse.service';
+import { OperatingService } from 'src/app/shared/services/operating/operating.service';
 
 @Component({
     selector: 'ffg-statistics-page',
@@ -7,6 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsPageComponent implements OnInit
 {
-    constructor() { }
-    public ngOnInit(): void { }
+    public stats: Array<any> = [];
+
+    constructor(public ereignisse: EreignisseService) { }
+    public async ngOnInit(): Promise<void>
+    {
+        const ereignisse = await this.ereignisse.getAll();
+        ereignisse.forEach(async ereigniss =>
+        {
+            const year = ereigniss.datetimeStart.getFullYear();
+            let index = this.stats.findIndex(e => e?.year === year);
+
+            if (index === -1)
+            {
+                this.stats.push({
+                    year,
+                    einsaetze: 0,
+                    fuellungen: 0
+                });
+
+                index = this.stats.length - 1;
+            }
+
+            this.stats[index].einsaetze++;
+            this.stats[index].fuellungen += ereigniss.fuellungen.length;
+        });
+
+        this.stats.sort((a: any, b: any) => b.year - a.year);
+    }
 }
